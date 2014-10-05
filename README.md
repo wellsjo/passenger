a037d3d574634fcabc78ae6287e8efd8c07312b0
Wells Johnston <wellsjohnston@gmail.com>
2014-10-05 18:12:24 -0400

    updated readme with install instructions

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-passenger
-=========
+## Passegner
+Messaging framework built around peerjs
 
-framework for peerjs 
+### Install
+```bash
+git clone git@github.com:wellsjo/passenger.git
+cd passenger.git
+./devify.sh # creates run.sh script
+```
+### Run Peer Server
+```bash
+./run.sh
+```
--- app/server/passenger-server-remote.js
+++ app/server/passenger-server-remote.js
@@ -5,14 +5,18 @@
 
 var PeerServer = require('peer').PeerServer;
 
+var host = '54.164.53.196';
+var port = 9000;
+
 var server = new PeerServer({
-    host: '54.164.53.196',
-    port: 9000,
+    host: host,
+    port: port,
     path: '/',
     allow_discovery: true
 });
 
-console.log('PeerServer listening on port 9000');
+console.log('PeerServer listening on port ' + port);
+console.log('Connect to: ' + host + ':' + port);
 
 // basic connection management
 var connections = [];
